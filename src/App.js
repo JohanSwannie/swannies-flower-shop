6e3e8ad7fe334d30215a6ef40b0c00dfaab1697d
jswan <jswannieman@gmail.com>
2023-02-22 20:15:39 +1100

    Update App component for a route to customers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Signin from "./routes/signin/signin.component";
+import Customer from "./routes/customer/customer.component";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth1" element={<Signin />} />
+        <Route path="/customer" element={<Customer />} />
       </Routes>
     </div>
   );
