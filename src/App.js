0987d4a53be5b851b7688eb9139a1cb288a57110
jswan <jswannieman@gmail.com>
2023-02-22 19:27:50 +1100

    Create a route in App Component for signin

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
 
 function App() {
-  return <div>Swannies Flower Shop</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth1" element={<Signin />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
