78c52b1168dd4ad85b808ca5e052c872da111da9
jswan <jswannieman@gmail.com>
2023-02-22 20:31:10 +1100

    Create a route in App Component for administrator

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./routes/home/home.component";
 import Signin from "./routes/signin/signin.component";
 import Customer from "./routes/customer/customer.component";
 import Checkout from "./routes/checkout/checkout.component";
+import Administrator from "./routes/administrator/administrator.component";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/auth1" element={<Signin />} />
         <Route path="/customer" element={<Customer />} />
         <Route path="/checkout" element={<Checkout />} />
+        <Route path="/admin" element={<Administrator />} />
       </Routes>
     </div>
   );
