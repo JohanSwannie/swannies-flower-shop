f700b3793b6a79fcf8bb0cf4558f8f5db467f77b
jswan <jswannieman@gmail.com>
2023-02-22 20:01:59 +1100

    Update index component to cater for context

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ProductsProvider } from "./context/products-context/products-context.component";
+import { CartProvider } from "./context/cart-context/cart-context.component";
+import { CustProvider } from "./context/cust-context/cust-context.component";
 import "./index.css";
 import App from "./App";
 
@@ -9,7 +12,13 @@ const rootElement = ReactDOM.createRoot(document.getElementById("root"));
 rootElement.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ProductsProvider>
+        <CartProvider>
+          <CustProvider>
+            <App />
+          </CustProvider>
+        </CartProvider>
+      </ProductsProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
