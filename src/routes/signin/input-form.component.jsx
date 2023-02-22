92f7209d8e1b9920234eb27439193f54f76a75a8
jswan <jswannieman@gmail.com>
2023-02-22 19:43:14 +1100

    Add input form component

---
+++ src/routes/signin/input-form.component.jsx
@@ -0,0 +1,16 @@
+import { FormInputLabel, Input, Group } from "./inputform.styles";
+
+const InputForm = ({ label, ...otherProps }) => {
+  return (
+    <Group>
+      <Input {...otherProps} />
+      {label && (
+        <FormInputLabel shrink={otherProps.value.length}>
+          {label}
+        </FormInputLabel>
+      )}
+    </Group>
+  );
+};
+
+export default InputForm;
