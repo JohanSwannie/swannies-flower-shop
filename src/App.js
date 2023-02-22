3341ac89fb36ce99a06e4cdc62f6581755ef891e
jswan <jswannieman@gmail.com>
2023-02-22 20:23:13 +1100

    Create a route in App Component for checkout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Signin from "./routes/signin/signin.component";
 import Customer from "./routes/customer/customer.component";
+import Checkout from "./routes/checkout/checkout.component";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/auth1" element={<Signin />} />
         <Route path="/customer" element={<Customer />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </div>
   );
