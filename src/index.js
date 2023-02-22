62d9542fd9d32fe784aba603cb550a05f3be8313
jswan <jswannieman@gmail.com>
2023-02-22 18:47:56 +1100

    Changes to App.js & index.js Components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
@@ -7,6 +8,8 @@ const rootElement = ReactDOM.createRoot(document.getElementById("root"));
 
 rootElement.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
